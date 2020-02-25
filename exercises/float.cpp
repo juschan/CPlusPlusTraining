e730d2256db20125dbb4b3813943a256938c65fc
juschan <justinchanuk@gmail.com>
2020-02-25 14:04:21 +0800

    Updated float.cpp to use iostream, iomanip etc.

--- exercises/float.cpp
+++ exercises/float.cpp
@@ -1,6 +1,6 @@
-#include <cstdio>
-//using namespace std;
-
+#include <iostream>
+#include <iomanip>
+using namespace std;
 
 int main(void) {
     puts("Floating point type usage in C:");
@@ -10,22 +10,24 @@ int main(void) {
     double			doubleNumber;
     
     // Size of floating point types
-    printf("Storage size for unsigned float : %lu  bytes \n", sizeof(float));
-    printf("Storage size for double :         %lu  bytes \n", sizeof(double));
+    cout << "Storage size for unsigned float : " << setprecision(0) << sizeof(float) << " bytes" << endl;
+    cout << "Storage size for double: " << setprecision(0) << sizeof(double) << " bytess" << endl;
     
     floatNumber = 2.0/3.0;
     doubleNumber = 2.0/3.0;
     
     puts("\nCompare precision at 4 decimal points:");
-    printf("floatNumber      = %1.4f\n", floatNumber);
-    printf("doubleNumber     = %1.4lf\n", doubleNumber);
+    cout << "floatNumber = " << setprecision(4) << floatNumber << endl;
+    cout << "doubleNumber = " << setprecision(4) << doubleNumber << endl;
     
+    std::setprecision(10);
     puts("\nCompare precision at 10 decimal points:");
-    printf("floatNumber      = %1.10f\n", floatNumber);
-    printf("doubleNumber     = %1.10lf\n", doubleNumber);
+    cout << "floatNumber = " << setprecision(10) << floatNumber << endl;
+    cout << "doubleNumber = " << setprecision(10) << doubleNumber << endl;
     
+    std::setprecision(30);
     puts("\nCompare precision at 30 decimal points:");
-    printf("floatNumber      = %1.30f\n", floatNumber);
-    printf("doubleNumber     = %1.30lf\n", doubleNumber);
+    cout << "floatNumber = " << setprecision(30) << floatNumber << endl;
+    cout << "doubleNumber = " << setprecision(30) << doubleNumber << endl;
     return 0;
 }
